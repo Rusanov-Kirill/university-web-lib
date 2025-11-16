daf11c523b03273bf0c6792cefb68612faf04a2a
Rusanov-Kirill <rusanovkirill39@gmail.com>
2025-11-17 00:06:36 +0600

    routes: add admin panel to routes

--- frontend/src/routes/appRoutes.js
+++ frontend/src/routes/appRoutes.js
@@ -3,14 +3,14 @@ import HomePage from '../pages/HomePage/HomePage'
 import BooksPage from '../pages/BooksPage/BooksPage'
 import GenresPage from '../pages/GenresPage/GenresPage'
 import AuthersPage from '../pages/AuthorsPage/AuthorsPage'
-import AuthPage from '../admin/pages/AuthPage/AuthPage'
+import AdminPanel from '../admin/pages/AdminPanel/AdminPanel'
 
 const appRoutes = [
   { path: '/', element: React.createElement(HomePage), index: true },
   { path: '/books', element: React.createElement(BooksPage)},
   { path: '/genres', element: React.createElement(GenresPage)},
   { path: '/authors', element: React.createElement(AuthersPage)},
-  { path: '/admin', element: React.createElement(AuthPage)},
+  { path: '/admin', element: React.createElement(AdminPanel)},
 ]
 
 export default appRoutes
