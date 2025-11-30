bc0dcc8550187302f72d45907fab365406249dd5
Rusanov-Kirill <rusanovkirill39@gmail.com>
2025-12-01 01:40:29 +0600

    routes: add admin-login page to routes

--- frontend/src/routes/appRoutes.js
+++ frontend/src/routes/appRoutes.js
@@ -4,12 +4,14 @@ import BooksPage from '../pages/BooksPage/BooksPage'
 import GenresPage from '../pages/GenresPage/GenresPage'
 import AuthersPage from '../pages/AuthorsPage/AuthorsPage'
 import AdminPanel from '../admin/pages/AdminPanel/AdminPanel'
+import AdminLogin from '../admin/pages/AdminLogin/AdminLogin'
 
 const appRoutes = [
   { path: '/', element: React.createElement(HomePage), index: true },
   { path: '/books', element: React.createElement(BooksPage)},
   { path: '/genres', element: React.createElement(GenresPage)},
   { path: '/authors', element: React.createElement(AuthersPage)},
+  { path: '/admin-login', element: React.createElement(AdminLogin)},
   { path: '/admin', element: React.createElement(AdminPanel)},
 ]
 
